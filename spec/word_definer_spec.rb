4f4abd440b54bfdd55cd3d087335bcd116916c4e
Drew Henderson <drewhendersonemail@gmail.com>
2021-12-17 10:25:46 -0800

    added second test for Word

--- spec/word_definer_spec.rb
+++ spec/word_definer_spec.rb
@@ -4,11 +4,20 @@ require 'pry'
 
 describe '#Word' do
 
-  
-
   describe('.all') do
     it("returns an empty array when there are no stored words") do
       expect(Word.all).to(eq([]))
     end
   end
+
+  describe('#save') do
+    it("saves a word") do
+      word = Word.new("Giant Steps", nil)
+      word.save()
+      word2 = Word.new("Blue", nil)
+      word2.save()
+      expect(Word.all).to(eq([word, word2]))
+    end
+  end
+
 end
